c54dc0714bf269287f447d5f0b6e3b5bea02db79
Dien Vu <meotimdihia@gmail.com>
2014-01-22 23:29:48 +0700

    updated

---
+++ src/com/example/hpswine/DashboardActivity.java
@@ -0,0 +1,68 @@
+package com.example.hpswine;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.view.InputDevice;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
+
+public class DashboardActivity extends Activity {
+
+	private static final String TAG = "LogLogLog";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_dashboard);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+	
+	public void detectIDheo(View v) {
+		Log.v(TAG, "onKeyup=" );
+		Toast.makeText(this, "ID: ", Toast.LENGTH_LONG).show();
+		FireMissilesDialogFragment Fire = new FireMissilesDialogFragment ();
+		Fire.show(getFragmentManager(), "Detect ID heo");
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.dashboard, menu);
+		return true;
+	}
+	
+	private static String codeid = "";
+	@Override
+    public boolean onKeyUp(int keyCode, KeyEvent event)
+    {
+		Toast.makeText(this, "ID: ", Toast.LENGTH_LONG).show();
+    	InputDevice input = event.getDevice();
+    	Log.v(TAG, "onKeyup=" );
+    	if (input != null) {
+	    	String descriptor = input.getDescriptor();
+	    	
+//	    	if (descriptor.equals("0cd6a2f865b1f4e6ef3e296aeee58bc95f7574af")) {
+	    		
+		    	if (keyCode != 66) {
+		    		char key = (char)event.getUnicodeChar();
+		    		codeid += key;
+		    	} else {
+		    		Toast.makeText(this, "ID: " + codeid, Toast.LENGTH_LONG).show();
+		    		codeid = "";
+		    	}
+//	    	}
+    	}
+
+    	return super.onKeyUp(keyCode, event);
+    }
+}
---
+++ src/com/example/hpswine/FireMissilesDialogFragment.java
@@ -0,0 +1,57 @@
+package com.example.hpswine;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnKeyListener;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.InputDevice;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.widget.Toast;
+
+
+public class FireMissilesDialogFragment extends DialogFragment{
+	
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		 getDialog().setOnKeyListener(new OnKeyListener()
+		 {
+		    public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event){
+		    	return true;
+		  }
+		});
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		this.setOnKeyListener(this);
+	}
+	
+	private void setOnKeyListener(
+			FireMissilesDialogFragment fireMissilesDialogFragment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+	    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+	    // Get the layout inflater
+	    LayoutInflater inflater = getActivity().getLayoutInflater();
+
+	    // Inflate and set the layout for the dialog
+	    // Pass null as the parent view because its going in the dialog layout
+	    builder.setView(inflater.inflate(R.layout.detect_heo, null));
+    
+	    return builder.create();
+	}
+
+
+}
